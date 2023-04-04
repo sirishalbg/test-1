df7beb339588d0755f0c8c50b4520a8ddf7f85d8
sirishalbg <sirisha.m@lloydsbanking.dev>
2023-04-04 20:40:46 +0530

    test

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+a=30
+b=10
+
+print("value of a before swap:", a)
+print("value of a before swap:", b)
+
+temp=a
+a=b
+b=temp
+
+print("value of a after swap:", a)
+print("value of a after swap:", b)
